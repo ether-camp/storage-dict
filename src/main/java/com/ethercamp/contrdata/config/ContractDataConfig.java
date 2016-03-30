d0ecfa9de8f755f332161bdb0707b87ab214d2c5
e.shevchenko <eugene@ethergit.com>
2016-03-30 16:30:22 +0300

    Fix config

--- src/main/java/com/ethercamp/contrdata/config/ContractDataConfig.java
+++ src/main/java/com/ethercamp/contrdata/config/ContractDataConfig.java
@@ -1,7 +1,10 @@
 package com.ethercamp.contrdata.config;
 
+import org.ethereum.config.SystemProperties;
 import org.ethereum.datasource.KeyValueDataSource;
 import org.ethereum.datasource.LevelDbDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +12,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan("com.ethercamp.contrdata")
 public class ContractDataConfig {
+
+    @Bean
+    public SystemProperties systemProperties() {
+        return SystemProperties.CONFIG;
+    }
 
     @Bean
     public KeyValueDataSource storageDict() {
